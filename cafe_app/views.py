898c409fadd09b9262b90fb4177c6dbcdfa40d20
Benex254 <benextempest@gmail.com>
2024-10-22 16:59:24 +0300

    feat: reconnect backend with frontend

--- cafe_app/views.py
+++ cafe_app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, "home.html")
+
+
+def contacts(request):
+    return render(request, "contacts.html")
+
+
+def menu(request):
+    return render(request, "menu.html")
+
+
+def about(request):
+    return render(request, "about.html")
+
+
+def team(request):
+    return render(request, "team.html")
