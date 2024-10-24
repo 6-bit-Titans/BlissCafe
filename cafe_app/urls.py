66cf8a4bc142308cbbce1297d51c7a21c2930c5f
TrevorCecil <trexgaming583@gmail.com>
2024-10-24 19:41:27 +0300

    feat: enable form functionality

--- cafe_app/urls.py
+++ cafe_app/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("home/", views.home, name="about"),
-    path("contacts/", views.contacts, name="about"),
-    path("team/", views.team, name="about"),
-    path("menu/", views.menu, name="about"),
+    path("home/", views.home, name="home"),
+    path("contacts/", views.contacts, name="contacts"),
+    path("team/", views.team, name="team"),
+    path("menu/", views.menu, name="menu"),
     path("about/", views.about, name="about"),
 ]
