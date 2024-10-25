9442e553e7b0daeea81022b3b07c49b78eeb7bdc
Benex254 <benextempest@gmail.com>
2024-10-25 20:26:12 +0300

    feat: add data

---
+++ cafe_app/data/home_data.py
@@ -0,0 +1,23 @@
+# genereate data on coffee name and price
+coffee_data = [
+    {"title": "expresso", "price": "Ksh. 2000"},
+    {"title": "expresso", "price": "Ksh. 2000"},
+    {"title": "expresso", "price": "Ksh. 2000"},
+    {"title": "expresso", "price": "Ksh. 2000"},
+    {"title": "expresso", "price": "Ksh. 2000"},
+    {"title": "expresso", "price": "Ksh. 2000"},
+    {"title": "expresso", "price": "Ksh. 2000"},
+    {"title": "expresso", "price": "Ksh. 2000"},
+    {"title": "expresso", "price": "Ksh. 2000"},
+]
+dessert_data = [
+    {"title": "Cake", "price": "Ksh. 6999"},
+    {"title": "Cake", "price": "Ksh. 6999"},
+    {"title": "Cake", "price": "Ksh. 6999"},
+    {"title": "Cake", "price": "Ksh. 6999"},
+    {"title": "Cake", "price": "Ksh. 6999"},
+    {"title": "Cake", "price": "Ksh. 6999"},
+    {"title": "Cake", "price": "Ksh. 6999"},
+    {"title": "Cake", "price": "Ksh. 6999"},
+    {"title": "Cake", "price": "Ksh. 6999"},
+]
